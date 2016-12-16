e6b9a82a0d260ff0eb598a786934cff2b1739949
Aloys Augustin <aloys.augustin@polytechnique.org>
2016-12-16 19:18:40 +0100

    Only one endpoint is simpler to configure

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import abort
+import pytz
 from suds.client import Client
 import json
 import binascii
@@ -17,47 +18,38 @@ SCALR_SIGNING_KEY = 'scalr signing key'
 import_url = 'http://server-diamondip/inc-ws/services/Imports?wsdl'
 delete_url = 'http://server-diamondip/inc-ws/services/Deletes?wsdl'
 
-@app.route('/addDevice/', methods=['POST'])
-def handle_addDev():
+@app.route("/", methods=['POST'])
+def webhook_listener():
     try:
         if not validateRequest(request):
             abort(403)
-        data = request.get_json(force=True)
-        client = Client(import_url, username=IPCONTROL_LOGIN, password=IPCONTROL_PASSWORD)
-        device = client.factory.create('WSDevice')
-        device.addressType = 'Static'
-        device.hostname = data['SCALR_EVENT_SERVER_HOSTNAME']
-        device.ipAddress = data['SCALR_EVENT_INTERNAL_IP']
-        """device = {
-            'addressType' : 'Reserved',
-            'hostname' : data['SCALR_EVENT_SERVER_HOSTNAME'],
-            'ipAddress': data['SCALR_EVENT_INTERNAL_IP']
-        }"""
-        client.service.importDevice(device)
-        return ""
+
+        data = json.loads(request.data)
+        if not 'eventName' in data or not 'data' in data:
+            abort(404)
+
+        if data['eventName'] == 'HostUp':
+            return addDev(data['data'])
+        elif data['eventName'] in ['HostDown', 'BeforeHostTerminate']:
+            return delDev(data['data'])
     except Exception as e:
         print(e)
         traceback.print_exc()
         abort(401)
 
-@app.route('/delDevice/', methods=['POST'])
-def handle_delDev():
-    try:
-        if not validateRequest(request):
-            abort(403)
-        data = request.get_json(force=True)
-        client = Client(delete_url, username=IPCONTROL_LOGIN, password=IPCONTROL_PASSWORD)
-        device = client.factory.create('WSDevice')
-        device.ipAddress = data['SCALR_EVENT_INTERNAL_IP']
-        """device = {
-            'ipAddress': data['SCALR_EVENT_INTERNAL_IP']
-        }"""
-        client.service.deleteDevice(device)
-        return ""
-    except Exception as e:
-        print(e)
-        traceback.print_exc()
-        abort(401)
+def addDev(data):
+    client = Client(import_url, username=IPCONTROL_LOGIN, password=IPCONTROL_PASSWORD)
+    device = client.factory.create('WSDevice')
+    device.addressType = 'Static'
+    device.hostname = getHostname(data)
+    device.ipAddress = data['SCALR_EVENT_INTERNAL_IP']
+    return client.service.importDevice(device)
+
+def delDev(data):
+    client = Client(delete_url, username=IPCONTROL_LOGIN, password=IPCONTROL_PASSWORD)
+    device = client.factory.create('WSDevice')
+    device.ipAddress = data['SCALR_EVENT_INTERNAL_IP']
+    return client.service.deleteDevice(device)
 
 def validateRequest(request):
     if not 'X-Signature' in request.headers or not 'Date' in request.headers:
@@ -71,6 +63,10 @@ def validateRequest(request):
     now = datetime.now(pytz.utc)
     delta = abs((now - date).total_seconds())
     return delta < 300
+
+def getHostname(data):
+    hostname = data['SCALR_EVENT_SERVER_HOSTNAME']
+    return hostname
 
 if __name__=='__main__':
     app.run(debug=False, host='0.0.0.0')
