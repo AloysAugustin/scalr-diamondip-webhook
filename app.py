307228f6fab39fafc4704b274856b62478cb6f95
Mohammed Hawari <mohammed@hawari.fr>
2016-12-16 17:15:24 +0100

    V2 Suds

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ IPCONTROL_PASSWORD = 'password'
 SCALR_SIGNING_KEY = 'scalr signing key'
 
 import_url = 'http://server-diamondip/inc-ws/services/Imports?wsdl'
-delete_url = 'http://server-diamondip//inc-ws/services/Deletes?wsdl'
+delete_url = 'http://server-diamondip/inc-ws/services/Deletes?wsdl'
 
 @app.route('/addDevice/', methods=['POST'])
 def handle_addDev():
@@ -24,11 +24,15 @@ def handle_addDev():
             abort(403)
         data = request.get_json(force=True)
         client = Client(import_url, username=IPCONTROL_LOGIN, password=IPCONTROL_PASSWORD)
-        device = {
+        device = client.factory.create('WSDevice')
+        device.addressType = 'Reserved'
+        device.hostname = data['SCALR_EVENT_SERVER_HOSTNAME']
+        device.ipAddress = data['SCALR_EVENT_INTERNAL_IP']
+        """device = {
             'addressType' : 'Reserved',
             'hostname' : data['SCALR_EVENT_SERVER_HOSTNAME'],
             'ipAddress': data['SCALR_EVENT_INTERNAL_IP']
-        }
+        }"""
         client.service.importDevice(device)
         return ""
     except Exception as e:
@@ -43,10 +47,12 @@ def handle_delDev():
             abort(403)
         data = request.get_json(force=True)
         client = Client(delete_url, username=IPCONTROL_LOGIN, password=IPCONTROL_PASSWORD)
-        device = {
+        device = client.factory.create('WSDevice')
+        device.ipAddress = data['SCALR_EVENT_INTERNAL_IP']
+        """device = {
             'ipAddress': data['SCALR_EVENT_INTERNAL_IP']
-        }
-        client.service.deleteDevice(client)
+        }"""
+        client.service.deleteDevice(device)
         return ""
     except Exception as e:
         print(e)
