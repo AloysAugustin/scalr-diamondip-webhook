2bcdcf81b4e53cdfd42e2616e9345b3f81014d59
Aloys Augustin <aloys.augustin@polytechnique.org>
2017-05-31 11:18:18 +0200

    Prod deployment

--- relaunch.sh
+++ relaunch.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
 docker stop webhook-diamondip
 docker rm webhook-diamondip
-docker build -t webhook-diamondip .
-docker run -p 5000:5000 -tid --name webhook-diamondip webhook-diamondip
+
+docker build -t webhook-diamondip \
+    --build-arg http_proxy=$(http_proxy) \
+    --build-arg https_proxy=$(https_proxy) \
+    .
+
+docker run -p 5000:5000 -tid  \
+    -v `pwd`/logs:/var/log \
+    --name webhook-diamondip \
+    webhook-diamondip
